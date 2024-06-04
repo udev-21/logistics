62ebd93dc9642d188292bd9fe14f56dcd447ab32
boburermonov <bobur.ermonov.97@mail.ru>
2024-06-04 10:25:38 +0500

    country and cities

--- api/logistics_services/views/logistics_services.py
+++ api/logistics_services/views/logistics_services.py
@@ -1,4 +1,6 @@
 from rest_framework import generics, pagination
+
+from api.core.pagination import PageNumberPagination
 from apps.logistics_services.models.services import LogisticsService
 from api.logistics_services.serializers.logistics_services import (
     LogisticsServiceSerializer,
@@ -9,7 +11,7 @@ from django_filters.rest_framework import DjangoFilterBackend
 class LogisticsServicesListView(generics.ListAPIView):
     queryset = LogisticsService.objects.all()
     serializer_class = LogisticsServiceSerializer
-
+    pagination_class = PageNumberPagination
     filter_backends = (DjangoFilterBackend,)
 
     filterset_fields = {
